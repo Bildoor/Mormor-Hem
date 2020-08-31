6f7ae350329ef1abb6402243336ba95b5243291e
Nicklas Hedlund <nicklas.hedlund@tieto.com>
2020-09-01 01:09:15 +0200

    remewed script

--- launch.bat
+++ launch.bat
@@ -1,3 +1,2 @@
-REM "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -kiosk -fullscreen C:\mormor-hem\index.html
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -fullscreen C:\mormor-hem\index.html
 
-"C:\Users\hedlvnic\AppData\Local\Google\Chrome SxS\Application\chrome.exe" --fullscreen "www.google.se"
